5049b699c53df9be90f4c5fc8d09f51b64bfaa73
Tzvi Friedman <tadwork@gmail.com>
2023-12-04 00:27:44 -0500

    day 3

--- 3/day3.py
+++ 3/day3.py
@@ -6,11 +6,11 @@ def create_matrix(schematic):
         matrix.append(list(line.strip()))
     return matrix
 
+## Part 1
 def has_symbol(matrix, row, col):
     if row<0 or col<0 or row>=len(matrix) or col>=len(matrix[row]):
         return False
     else:
-        print(matrix[row][col])
         return not matrix[row][col].isnumeric() and matrix[row][col] != '.'
     
 def check_for_symbol_and_return_part(matrix, row, begin, end):
@@ -40,10 +40,61 @@ def sum_of_partnumbers(schematic):
             col+=1
         row+=1
     return total
-                
+
+## Part 2
+def get_full_part_number_from_coord(matrix,row,col):
+    begin = col
+    while begin >=0 and matrix[row][begin].isnumeric():
+        begin-=1
+    end=col
+    while end < len(matrix[row]) and matrix[row][end].isnumeric():
+        end+=1
+    return int(''.join(matrix[row][begin+1:end])),end
+
+def is_part_num(matrix, row, col):
+    if row<0 or col<0 or row>=len(matrix) or col>=len(matrix[row]):
+        return False
+    else:
+        return matrix[row][col].isnumeric()
+    
+def get_prod_of_adjacent_part_numbers_to_gear(matrix, row, col):
+    part_numbers = []
+    c=col-1
+    while c < col+2:
+        if is_part_num(matrix,row+1,c):
+            num, end = get_full_part_number_from_coord(matrix,row+1,c)
+            part_numbers.append(num)
+            c=end
+        else:
+            c+=1
+    c=col-1
+    while c < col+2:
+        if is_part_num(matrix,row-1,c):
+            num, end = get_full_part_number_from_coord(matrix,row-1,c)
+            part_numbers.append(num)
+            c=end
+        else:
+            c+=1
+    if is_part_num(matrix,row,col-1):
+        part_numbers.append(get_full_part_number_from_coord(matrix,row,col-1)[0])
+    if is_part_num(matrix,row,col+1):
+        part_numbers.append(get_full_part_number_from_coord(matrix,row,col+1)[0])
+    if len(part_numbers) == 2:
+        return part_numbers[0] * part_numbers[1]
+    return 0
+
+def get_all_gear_coordinates(schematic):
+    matrix = create_matrix(schematic)
+    total = 0
+    for i,row in enumerate(matrix):
+        for j,_ in enumerate(row):
+            if matrix[i][j] == '*':
+                total+=get_prod_of_adjacent_part_numbers_to_gear(matrix,i,j)
+    return total
             
 if __name__ == '__main__':
     with open('3/schematic.txt', encoding='utf-8') as f:
         schematic = f.read()
         print(sum_of_partnumbers(schematic))
+        print(get_all_gear_coordinates(schematic))
     
--- 3/test_day3.py
+++ 3/test_day3.py
@@ -1,8 +1,15 @@
 
 import pytest
 
-from day3 import create_matrix, has_symbol, check_for_symbol_and_return_part,sum_of_partnumbers
-
+from day3 import (
+    create_matrix, 
+    has_symbol, 
+    check_for_symbol_and_return_part,sum_of_partnumbers,
+    is_part_num, 
+    get_full_part_number_from_coord, 
+    get_prod_of_adjacent_part_numbers_to_gear, 
+    get_all_gear_coordinates
+)
 def test_create_matrix():
     schematic = """
     1.2
@@ -70,3 +77,76 @@ def test_sum_of_partnumbers():
 .664.598..
 """
     sum_of_partnumbers(schematic) ==  4361
+
+def test_is_part_num():
+    matrix = [
+        ['1','*','2'],
+        ['3','.','4']
+    ]
+    assert is_part_num(matrix,0,0) == True
+    assert is_part_num(matrix,0,1) == False
+    assert is_part_num(matrix,-1,0) == False
+    assert is_part_num(matrix,2,1) == False
+    
+def test_get_full_part_number_from_coord():
+    matrix = [
+        ['.','.','.'],
+        ['1','1','1'],
+        ['3','*','4']
+    ]
+    assert get_full_part_number_from_coord(matrix,1,1)[0] == 111
+    assert get_full_part_number_from_coord(matrix,2,0)[0] == 3
+    assert get_full_part_number_from_coord(matrix,2,2)[0] == 4
+    
+def test_get_prod_of_adjacent_part_numbers_to_gear():
+    # matrix = [
+    #     ['.','.','.'],
+    #     ['.','.','.'],
+    #     ['3','*','4']
+    # ]
+    # assert get_prod_of_adjacent_part_numbers_to_gear(matrix, 2, 1) == 12
+    matrix = [
+        ['.','.','.'],
+        ['.','1','0'],
+        ['3','*','.']
+    ]
+    assert get_prod_of_adjacent_part_numbers_to_gear(matrix, 2, 1) == 30
+    matrix = [
+        ['.','.','.'],
+        ['.','*','.'],
+        ['3','.','2']
+    ]
+    assert get_prod_of_adjacent_part_numbers_to_gear(matrix, 1, 1) ==6
+    matrix = [
+        ['.','*','1'],
+        ['.','.','.'],
+        ['3','.','2']
+    ]
+    assert get_prod_of_adjacent_part_numbers_to_gear(matrix, 0, 1) == 0
+    matrix = [
+        ['.','.','1'],
+        ['.','*','.'],
+        ['3','.','2']
+    ]
+    assert get_prod_of_adjacent_part_numbers_to_gear(matrix, 1, 1) == 0
+    matrix = [
+        ['.','.','.'],
+        ['.','*','.'],
+        ['2','.','2']
+    ]
+    assert get_prod_of_adjacent_part_numbers_to_gear(matrix, 1, 1) == 4
+    
+def test_get_all_gear_coordinates():
+    schematic = """
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+"""
+    assert get_all_gear_coordinates(schematic) == 467835
