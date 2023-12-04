1dc53b2f57c39e6c0cc6189925c49b701682ac5f
Tzvi Friedman <tadwork@gmail.com>
2023-12-04 14:08:04 -0500

    Day 4 part 2

--- 4/day4.py
+++ 4/day4.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 
 def score_from_diff(score):
     if score == 1:
@@ -9,16 +10,31 @@ def score_from_diff(score):
     
 def parse_card_input(card):
     name, puzzle = card.split(':')
+    card_id = int(name.split()[1])
     winning, yours = puzzle.split('|')
     winning_list, yours_list = winning.strip().split(), yours.strip().split()
     diff = len(winning_list) + len(yours_list) - len(set(winning_list + yours_list))
-    return winning_list, yours_list, diff
-    
-def process_cards(cards):
-    cards = [parse_card_input(card) for card in cards.splitlines() if card.strip()]
-    return sum([ score_from_diff(diff) for win, yours, diff in cards])
+    return card_id, diff
 
+def process_cards_and_return_score(cards):
+    cards = [parse_card_input(card) for card in cards.splitlines() if card.strip()]
+    return sum([ score_from_diff(diff) for card_id, diff in cards])
+
+## Part 2
+
+def process_cards_and_return_count(cards):
+    cards = [parse_card_input(card) for card in cards.splitlines() if card.strip()]
+    card_counts = defaultdict(int)
+    for card_id, diff in cards:
+        card_counts[card_id] += 1
+        for i in range(1,diff+1):
+            card_counts[card_id + i] += card_counts[card_id]
+        # print(card_id, diff,card_counts)
+    return sum(card_counts.values())
+        
+            
 if __name__ == '__main__':
     with open('4/puzzles.txt',mode='r', encoding='utf-8') as f:
         cards = f.read()
-    print(process_cards(cards))
+    print(process_cards_and_return_score(cards))
+    print(process_cards_and_return_count(cards))
--- 4/test_day4.py
+++ 4/test_day4.py
@@ -4,13 +4,14 @@ import pytest
 from day4 import (
     parse_card_input, 
     score_from_diff,
-    process_cards
+    process_cards_and_return_score,
+    process_cards_and_return_count
 )
 def test_parse_card_input():
     card1 = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
-    assert parse_card_input(card1) == (['41', '48', '83', '86', '17'], ['83', '86', '6', '31', '17', '9', '48', '53'],4)
+    assert parse_card_input(card1) == (1,4)
     cars2= "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19"
-    assert parse_card_input(cars2) == (['13', '32', '20', '16', '61'], ['61', '30', '68', '82', '17', '32', '24', '19'],2)
+    assert parse_card_input(cars2) == (2,2)
     
     
 def test_score_from_diff():
@@ -28,4 +29,17 @@ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
 """
-    assert process_cards(cards) ==  13
+    assert process_cards_and_return_score(cards) ==  13
+    
+def test_process_cards_and_return_count():
+    cards = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
+    assert process_cards_and_return_count(cards) ==  5
+    cards = """
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+"""
+    assert process_cards_and_return_count(cards) ==  30
