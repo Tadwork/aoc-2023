ad0a8ee237a5eb2d9424b2a6513eda6037f892e7
Tzvi Friedman <tadwork@gmail.com>
2023-12-05 13:45:34 -0500

    Day 5 part 1

---
+++ 5/day5.py
@@ -0,0 +1,59 @@
+class InstructionRange():
+    def __init__(self) -> None:
+        self.ranges = {}
+    def add(self,source_start, dest_start,length):
+        self.ranges[source_start] = {'dest_start':dest_start,'length':length}
+    
+    def get(self, source):
+        for start,rng in self.ranges.items():
+            if source >= start and source < start + rng['length']:
+                return source - start + rng['dest_start']
+        return source
+    
+
+class Almanac:
+    seeds = []
+    seed_to_soil = InstructionRange()
+    soil_to_fertilizer = InstructionRange()
+    fertilizer_to_water = InstructionRange()
+    water_to_light = InstructionRange()
+    light_to_temperature = InstructionRange()
+    temperature_to_humidity = InstructionRange()
+    humidity_to_location = InstructionRange()
+    def __init__(self, input:str) -> None:
+        lines = input.splitlines()
+        line_num = 0
+        while line_num < len(lines):
+            line = lines[line_num].strip()
+            if not line:
+                line_num+=1
+                continue
+            if line.startswith('seeds:'):
+                seed_list = line.split(':')[1].strip()
+                self.seeds = [int(s) for s in seed_list.split()]
+            elif 'map' in line:
+                property_name = line.split()[0].replace('-','_')
+                prop = getattr(self,property_name)
+                line_num+=1
+                while line_num < len(lines) and lines[line_num].strip():
+                    dest,source,count = lines[line_num].split()
+                    prop.add(int(source), int(dest), int(count))
+                    line_num+=1
+            line_num+=1
+            
+    def get_location(self,seed):
+        soil = self.seed_to_soil.get(seed)
+        fertilizer = self.soil_to_fertilizer.get(soil)
+        water = self.fertilizer_to_water.get(fertilizer)
+        light = self.water_to_light.get(water)
+        temperature = self.light_to_temperature.get(light)
+        humidity = self.temperature_to_humidity.get(temperature)
+        location = self.humidity_to_location.get(humidity)
+        return location
+    
+if __name__ == '__main__':
+    with open('5/almanac.txt',mode='r', encoding='utf-8') as f:
+        almanac = Almanac(f.read())
+        min_location = min([almanac.get_location(seed) for seed in almanac.seeds])
+        print(len(almanac.seeds))
+        print(min_location)
---
+++ 5/test_day5.py
@@ -0,0 +1,59 @@
+import pytest
+import day5
+test_input = \
+"""
+seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+"""
+
+class TestAlmanac:
+    def test_parse_input(self):
+        input = """
+        seeds: 79 14 55 13
+
+        seed-to-soil map:
+        50 98 2
+        """
+        almanac = day5.Almanac(input)
+        assert almanac.seeds == [79,14,55,13]
+        assert almanac.seed_to_soil.get(98) == 50
+        
+    def test_get_location(self):
+        almanac = day5.Almanac(test_input)
+        assert almanac.get_location(79) == 82
+        assert almanac.get_location(14) == 43
+        assert almanac.get_location(55) == 86
+        assert almanac.get_location(13) == 35
+    
+    
